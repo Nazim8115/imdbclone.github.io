69923092c34d84250c85447e1f7c49d7cb2d2af0
MOH NAZIM <nazim.nn459@gmail.com>
2023-05-21 11:10:56 +0530

    imdbclone done
    make a imdb clone web project

---
+++ movie_details.js
@@ -0,0 +1,36 @@
+"use strict";
+(function () {
+  const title = document.getElementById("title");
+  title.innerHTML = localStorage.getItem("movieName");
+  const year = document.getElementById("year");
+  const runtime = document.getElementById("duration");
+  const rating = document.getElementById("rating");
+  const poster = document.getElementById("poster");
+  const plot = document.getElementById("plot");
+  const directorsName = document.getElementById("director-names");
+  const castName = document.getElementById("cast-names");
+  const genre = document.getElementById("genre");
+
+  fetchMovies(title.innerHTML);
+
+  async function fetchMovies(search) {
+    const url = `https://www.omdbapi.com/?t=${search}&type=movie&apikey=e34ccfe1`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      title.innerHTML = data.Title; //change this
+
+      year.innerHTML = data.Year;
+      runtime.innerHTML = data.Runtime;
+      rating.innerHTML = `${data.imdbRating}/10`;
+      poster.setAttribute("src", `${data.Poster}`);
+      plot.innerHTML = data.Plot;
+      directorsName.innerHTML = data.Director;
+      castName.innerHTML = data.Actors;
+      genre.innerHTML = data.Genre;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+})();
